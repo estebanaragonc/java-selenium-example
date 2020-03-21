32cccec755eb91e404323937e333052667b89f73
Elvis Aragon <Elvis.X.Aragon.-ND@disney.com>
2020-03-21 13:23:08 -0500

    first 3 test cases added

--- src/test/java/test/TestMethods.java
+++ src/test/java/test/TestMethods.java
@@ -10,6 +10,93 @@ import pages.Itinerary;
 
 public class TestMethods extends BaseTest{
 
+	@Test(description = "TC1 - Validate results in grid style")
+    public void TC1_ValidateCruiseResultListInGridStyle() {
+		
+		//page objects
+        Home home = new Home(getDriver());
+        CruiseSearch cruiseSearch = new CruiseSearch(getDriver());
+        
+        //click close in modal opened
+        home.dismissModals();
+        //set the filters in home page
+        home.clickSailTo();     
+        home.selectSailOptionByText("The Bahamas");
+        home.clickSailFrom();
+        //home.selectRandomEnabledSailFromOption();
+        home.selectSailFromOptionByText("Baltimore, MD");
+        home.clickDates();
+        home.selectAvailableMonthsFromYear("2021");
+        home.clickDuration();
+        home.selectDurationByRange("6 - 9 Days");
+        //click search cruises
+        home.clickSearchCruises();
+        
+        //validation if grid is activated
+        Assert.assertTrue(cruiseSearch.isGridSelected());        
+    }
 	
+	@Test(description = "Validate the result list are affected by the 'price' filter")
+	public void TC2_ValidateResultsListIsAffectedByThePriceFilter()
+	{
+		
+		//page objects
+        Home home = new Home(getDriver());
+        CruiseSearch cruiseSearch = new CruiseSearch(getDriver());        
+        //click close in modal opened
+        home.dismissModals();
+        //set the filters in home page
+        home.clickSailTo();     
+        home.selectSailOptionByText("The Bahamas");
+        home.clickSailFrom();
+        //home.selectRandomEnabledSailFromOption();
+        home.selectSailFromOptionByText("Charleston, SC");
+        home.selectSailFromOptionByText("Baltimore, MD");
+        home.clickDates();
+        home.selectAvailableMonthsFromYear("2021");
+        home.clickDuration();
+        home.selectDurationByRange("6 - 9 Days");
+        //click search cruises
+        home.clickSearchCruises();
+        
+        // count number of cards displayed in grid
+        int totalResultList = cruiseSearch.countTotalCruisesResults();
+        int lastCardAmount = cruiseSearch.getLastCardAmount();
+        //click in the link 'pricing' in the filter section
+        cruiseSearch.clickPricing();
+        //move the filter to dismiss last card. Example if last card amount = 530, move filter to 520 and get new result
+        cruiseSearch.adjustMaximumPricePerPersonTo(lastCardAmount);
+        
+        int resultListUpdated = cruiseSearch.countTotalCruisesResults();
+        
+        //validate the last card was deleted in the result, they should not be the same
+        Assert.assertNotEquals(resultListUpdated, totalResultList);                
+	}
+	
+	@Test(description = "Validate the result list is sorted by price")
+	public void TC3_ValidateTheResultListIsSortedByPrice()
+	{
+		//page objects
+        Home home = new Home(getDriver());
+        CruiseSearch cruiseSearch = new CruiseSearch(getDriver());        
+        //click close in modal opened
+        home.dismissModals();
+        //set the filters in home page
+        home.clickSailTo();     
+        home.selectSailOptionByText("The Bahamas");
+        home.clickSailFrom();
+        //home.selectRandomEnabledSailFromOption();
+        home.selectSailFromOptionByText("Charleston, SC");
+        home.selectSailFromOptionByText("Baltimore, MD");
+        home.clickDates();
+        home.selectAvailableMonthsFromYear("2021");
+        home.clickDuration();
+        home.selectDurationByRange("6 - 9 Days");
+        //click search cruises
+        home.clickSearchCruises();
+        
+        // Validate the result list is sorted by price
+        Assert.assertTrue(cruiseSearch.isResultListSortedByPrice());
+	}
 
 }
